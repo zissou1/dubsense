05064c38b7ffd4b98843248ed4288ba2068aa737
zissou1 <arnqvist@me.com>
2024-10-13 22:38:20 +0200

    Updated gitignore

--- pyinstaller_build.bat
+++ pyinstaller_build.bat
@@ -1,3 +1,4 @@
+
 pushd %~dp0
 pyinstaller --onefile --noconsole --additional-hooks-dir=. --icon=dubsense.ico --add-binary "C:\Users\arnqv\AppData\Local\Programs\Python\Python310\Lib\site-packages\paddle\libs\mklml.dll;." dubsense.py
 popd
